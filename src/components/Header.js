82349cf0d021f8caec69adb50a95dc4e3baa06ba
Kieran <kieranpoconnell@gmail.com>
2022-11-01 21:08:08 -0230

    Added image assets, added content to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,46 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-width: 100%;
-background-color: #fff200;
-height: 5.5rem;;
-`
+  display: flex;
+  width: 100%;
+  background-color: #0d47a1;
+  height: 5.5rem; 
+  justify-content: space-between; 
+`;
 
+const Logo = styled.img`
+  display: block;
+  max-width: 20rem;
+  max-height: 10rem;
+ width: auto;
+ height: auto;
+ padding: 4px;
+`;
+const HeaderItemCtn = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderItem = styled.button`
+height: fit-content;
+border: none;
+background-color: transparent;
+font-size: 16px;
+color: #fff;
+`;
 
 const Header = () => {
   return (
-    <Wrapper>Header</Wrapper>
-  )
-}
+    <Wrapper>
+      <Logo src="./assets/bmechlogo.png" />
+      <HeaderItemCtn>
+        <HeaderItem>Contacts</HeaderItem>
+        <HeaderItem>Products</HeaderItem>
+        <HeaderItem>Services</HeaderItem>
+      </HeaderItemCtn>
+    </Wrapper>
+  );
+};
 
-export default Header
+export default Header;
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -5,7 +5,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    background: #014493;
+    background: #fff;
     font-family: Open-Sans, Helvetica, Sans-Serif;
   }
 `;
