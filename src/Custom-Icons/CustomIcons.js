23c0e1877179e87350040e507fa8e3c1059bb58e
Kieran <kieranpoconnell@gmail.com>
2022-11-01 23:13:42 -0230

    Added custom icons

---
+++ src/Custom-Icons/CustomIcons.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import { ReactComponent as Email } from "./email-svgrepo-com.svg";
+import { ReactComponent as Phone } from "./phone-call-svgrepo-com.svg";
+
+export const StyledPhone = styled(Phone)`
+  height: 2rem;
+  width: 2rem;
+  min-height: 1rem;
+  min-width: 1rem;
+  fill: rgb(46, 48, 145);
+`;
+
+export const StyledEmail = styled(Email)`
+  height: 2rem;
+  width: 2rem;
+  fill: rgb(46, 48, 145);
+  min-height: 1rem;
+  min-width: 1rem;
+`;
