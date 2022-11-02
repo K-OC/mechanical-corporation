729a07e3e8a1daf4c8628c7e36f029012c7b260e
Kieran <kieranpoconnell@gmail.com>
2022-11-01 23:14:16 -0230

    Installed React Router dom, initialized routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'; 
 import styled from 'styled-components';
+import { Route, Routes } from "react-router-dom";
 import GlobalStyle from './globalStyles';
 import Header from './components/Header';
+import Contact from './components/Contact';
+import Home from './components/Home';
 
 const Wrapper = styled.div`
 background-color: #fff;
-
 `
 
 function App() {
@@ -14,7 +16,11 @@ function App() {
    <GlobalStyle/>
    <Header></Header>
    <Wrapper>
-
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="contact" element={<Contact/>}/>
+    </Routes>
+    
    </Wrapper>
    </>
   );
