db81d0ae3053920d01c311836419a708cbfa6f8e
Kieran <kieranpoconnell@gmail.com>
2023-01-25 13:12:18 -0330

    products-and-services-page

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,9 +4,37 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: flex-start;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  max-width: 100%;
+  width: 70%;
+  height: 70%;
+  display: flex;
   align-items: center;
-  height: 100%;
-  width: 50%;
+  @media (max-width: 380px) {
+    height: calc(100% - 6rem);
+  }
+`;
+const PageTitle = styled.h2`
+  color: rgb(46, 48, 145);
+  font-size: 1.5rem;
+  @media (max-width: 1200px) {
+    font-size: 1rem;
+  }
+
+`
+const InnerCtn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  jusitify-content: center;
+  overflow-wrap: break-word;
+  flex-wrap: wrap;
+  width: 60%;
+  max-width: 100%;
+  background-color: transparent;
+  padding: 1rem;
   @media (max-width: 1200px) {
     width: 100%;
     margin: 0;
@@ -14,34 +42,41 @@ const Wrapper = styled.div`
 `;
 
 const Title = styled.h2`
-  color: rgb(46, 48, 145);
-`;
-const ContentCtn = styled.div`
-  display: flex;
-  justify-content: center;
-  background-color: #fff;
+  color: #fff;
+  font-size: 1em;
+  height: 3rem;
+  width: 5rem;
+  padding: 1rem;
+  text-align: left;
+  display: flex; /* added this line*/
+  align-items: center;
+  jusitify-content: center;
+  background-color: rgb(46, 48, 145);
   width: 50%;
   @media (max-width: 1200px) {
-    width: 100%;
-    margin: 0;
-  }
-  ul {
-    color: rgb(46, 48, 145);
-    font-weight: bold;
+    font-size: 1rem;
   }
 `;
 
 const Services = () => {
   return (
     <Wrapper>
-      <Title>Products & Services</Title>
-      <ContentCtn>
-        <ul>
-          <li>One</li>
-          <li>One</li>
-          <li>One</li>
-        </ul>
-      </ContentCtn>
+      <InnerCtn>
+        <PageTitle>Products & Services</PageTitle>
+        <InnerCtn>
+          <Title>Bit sharpening</Title>
+          <Title> Bucket Rebuilds</Title>
+          <Title> Drill Consumable Products</Title>
+          <Title> Drill Hammer Rebuilds</Title>
+          <Title> Hydraulic Hoses & Fittings</Title>
+          <Title> Laser Alignment</Title>
+          <Title> Line Boring</Title>
+          <Title> Mobile Welding & Mechanical Maintenance</Title>
+          <Title> Precision Machining</Title>
+          <Title> Truck Box Liners (Hardox)</Title>
+          <Title> Welding and Fabrication</Title>
+        </InnerCtn>
+      </InnerCtn>
     </Wrapper>
   );
 };
